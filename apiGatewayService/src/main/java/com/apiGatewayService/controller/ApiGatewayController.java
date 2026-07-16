a3ee2bffa36f8bbdc357b3c47c1276bde79ecee6
Amit Hari <amithari25@gmail.com>
2026-07-16 12:29:12 +0530

    logger

--- apiGatewayService/src/main/java/com/apiGatewayService/controller/ApiGatewayController.java
+++ apiGatewayService/src/main/java/com/apiGatewayService/controller/ApiGatewayController.java
@@ -5,20 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Logger;
 
 @RestController
 public class ApiGatewayController {
+    private static final Logger LOGGER = Logger.getLogger(ApiGatewayController.class.getName());
 
     @Value("${server.port}")
     private String port;
 
     @GetMapping("/gateway-info")
     public String info() {
+        LOGGER.info("Fetching gateway info: " + System.getenv("HOSTNAME"));
         return System.getenv("HOSTNAME");
     }
 
     @GetMapping("/test")
-    public String test() throws Exception {
+    public String test() throws UnknownHostException {
+        LOGGER.info("Testing gateway functionality: " + InetAddress.getLocalHost().getHostName());
         return "Served by : " + InetAddress.getLocalHost().getHostName()
                 + " Port : " + port;
     }
